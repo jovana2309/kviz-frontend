10ab63843e52989c44a552e36a885c90d4ecdaa5
Jovana <jovanastanicc@gmail.com>
2024-10-30 16:02:03 +0100

    Redirect when token expires

--- src/services/axios.js
+++ src/services/axios.js
@@ -28,7 +28,7 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   (error) => {
-    if (error.response && error.response.status === 401) {
+    if (error.response && (error.response.status === 401 || error.response.status === 403) ) {
      
       window.location.href = "/login";
     }
