ea3cf54e5810207e6c11a763f11fd589680dfd9c
Jovana <jovanastanicc@gmail.com>
2024-11-04 14:53:32 +0100

    FIXED ENV VAR

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
